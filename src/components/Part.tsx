45989239a8a7b339fe062d3cf58b6af379569341
vanessadasilvasantos <vanessadasilvasantos65@gmail.com>
2026-05-14 22:58:46 -0300

    Adicionada fotos das pecas

--- src/components/Part.tsx
+++ src/components/Part.tsx
@@ -1,15 +1,75 @@
 import Image from "next/image";
 
-export default function Part() {
+type ItemType = {
+  text: string;
+  highlights?: string[];
+};
+
+type PartProps = {
+  img: string;
+  alt: string;
+  title: string;
+  items?: ItemType[];
+};
+
+function highlightText(
+  text: string,
+  highlights: string[] = []
+) {
+  const regex = new RegExp(
+    `(${highlights.join("|")})`,
+    "gi"
+  );
+
+  const parts = text.split(regex);
+
+  return parts.map((part, index) =>
+    highlights.some(
+      (word) =>
+        word.toLowerCase() === part.toLowerCase()
+    ) ? (
+      <span
+        key={index}
+        className="font-bold text-[var(--l2)]"
+      >
+        {part}
+      </span>
+    ) : (
+      part
+    )
+  );
+}
+
+export default function Part({
+  img,
+  alt,
+  title,
+  items = [],
+}: PartProps) {
   return (
-    <div className="bg-[var(--c7)] flex rounded-[50px] w-[600px] p-[10px]">
-        <Image className="rounded-[50px]" src="/cavalo.png" alt="Malha do tabuleiro" width={200} height={200} />
-      <div className="pl-[20px]">
-        <h2 className="font-unica text-[var(--c1)] text-[40px]">Peão</h2>
-        <ul className="list-disc text-inter pl-[20px] text-[var(--c1)]">
-          <li>Item 1</li>
-          <li>Item 2</li>
-          <li>Item 3</li>
+    <div className="bg-[var(--c7)] flex rounded-[50px] w-[480px] p-[10px] gap-5">
+      <Image
+        className="rounded-[50px] object-cover"
+        src={img}
+        alt={alt}
+        width={200}
+        height={200}
+      />
+
+      <div className="flex flex-col">
+        <h2 className="font-unica text-[var(--c1)] pb-[10px] text-[40px]">
+          {title}
+        </h2>
+
+        <ul className="list-disc text-inter pl-[20px] text-[var(--c1)] flex flex-col gap-4">
+          {items.map((item, index) => (
+            <li key={index}>
+              {highlightText(
+                item.text,
+                item.highlights
+              )}
+            </li>
+          ))}
         </ul>
       </div>
     </div>
--- src/sections/Parts.tsx
+++ src/sections/Parts.tsx
@@ -3,7 +3,25 @@ import Part from "@/components/Part";
 export default function Parts() {
   return (
     <div className="flex flex-col gap-12 mt-[100px]">
-      <Part />
+      <Part
+        img="/peao.png"
+        alt="Peão"
+        title="Peão"
+        items={[
+          {
+            text: "Avança uma casa por vez.",
+            highlights: ["uma casa"],
+          },
+          {
+            text: "Captura em diagonal.",
+            highlights: ["diagonal"],
+          },
+          {
+            text: "Pode virar outra peça.",
+            highlights: ["virar"],
+          },
+        ]}
+      />
     </div>
   );
 }
