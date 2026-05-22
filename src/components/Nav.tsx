275e67fb3151cd89bbb93523bc0c726c34ae9a7f
vanessadasilvasantos <vanessadasilvasantos65@gmail.com>
2026-05-22 16:22:23 -0300

    Aplicada responsividade na hero

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,6 @@
 export default function Nav() {
   return (
-    <nav className="flex justify-between mx-[140px] mt-[40px] items-end">
+    <nav className="flex justify-between mx-[140px] max-lg:mx-[80px] max-md:mx-[40px] max-sm:mx-[20px] mt-[40px] max-sm:mt-[20px] items-end ">
       <a className="font-unica text-lg text-[var(--c6)]">Peças</a>
       <div className=" h-[1px] w-[20%] bg-[var(--m5)]"></div>
 
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -2,13 +2,13 @@ import Image from 'next/image';
 
 export default function Hero() {
   return (
-    <main className="flex mt-[100px] relative justify-between">
-      <Image src="/cavalo.png" alt="Cavalo do xadrez" width={350} height={64} className="ml-[140px] mb-4 mt-[30px]" />
+    <main className="flex mt-[100px] max-md:mt-[60px] relative justify-between  ">
+      <Image src="/cavalo.png" alt="Cavalo do xadrez" width={350} height={64} className="ml-[140px] max-lg:ml-[80px] max-md:ml-[40px] max-sm:ml-[0px] mb-4 mt-[30px] max-lg:w-[260px] max-md:w-[200px] max-sm:w-[150px]" />
 
-      <div className="">
-        <h1 className="text-[120px] mt-[80px] flex gap-4 mr-12 font-unica text-[var(--c6)]">XA<span></span>DR<span></span>EZ</h1>
-      <span className="text-[26px] mr-12 text-[var(--l3)] font-unica text-end block">chess</span>
-        <Image className="absolute top-0 right-0" src="/Malha.png" alt="Malha do tabuleiro" width={600} height={400} />
+      <div>
+        <h1 className="text-[120px] max-lg:text-[100px] max-md:text-[80px] mt-[80px] max-md:mt-[60px] flex gap-4 max-lg:gap-2 mr-12 max-lg:mr-10 font-unica text-[var(--c6)] max-sm:mr-7 max-sm:mt-[40px] max-sm:gap-1 max-sm:text-[60px]">XA<span></span>DR<span></span>EZ</h1>
+      <span className="text-[26px] max-md:text-[20px] mr-12 max-lg:mr-10 text-[var(--l3)] font-unica text-end block max-sm:mr-7">chess</span>
+        <Image className="absolute top-0 right-0 max-lg:w-[500px] max-md:w-[400px] max-sm:w-[300px]" src="/Malha.png" alt="Malha do tabuleiro" width={600} height={400} />
       </div>
     </main>
   );
